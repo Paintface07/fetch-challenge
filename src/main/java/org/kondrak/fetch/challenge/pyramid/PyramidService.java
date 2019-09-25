0e89ff35cd1a9d7ed1b5ea8b3502fb9ec78accf8
Kevin Kondratuk <Paintface07@gmail.com>
2019-09-24 21:08:44 -0500

    start splitting up the string and checking character counts.  also add a non-pyramid duplicate character string to the test constants

--- src/main/java/org/kondrak/fetch/challenge/pyramid/PyramidService.java
+++ src/main/java/org/kondrak/fetch/challenge/pyramid/PyramidService.java
@@ -1,11 +1,24 @@
 package org.kondrak.fetch.challenge.pyramid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 @Service
 public class PyramidService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PyramidService.class);
+
     public boolean isPyramidWord(String word) {
+        HashSet<String> chars = new HashSet<>(Arrays.asList(word.split("")));
+
+        for(String s : chars) {
+            LOGGER.info("Letter in word: {} - occurs {} times", s, StringUtils.countOccurrencesOf(word, s));
+        }
 
         return true;
     }
--- src/test/java/org/kondrak/fetch/challenge/PyramidTestConstants.java
+++ src/test/java/org/kondrak/fetch/challenge/PyramidTestConstants.java
@@ -4,9 +4,10 @@ package org.kondrak.fetch.challenge;
  * A class containing constants to be used in testing of the pyramid test endpoint.
  */
 final class PyramidTestConstants {
-    static String VALID_PYRAMID_WORD = "banana";    // Positive test data supplied
-    static String INVALID_PYRAMID_WORD = "bandana"; // Negative test data supplied
-    static String EMPTY_STRING = "";                // Empty test data (error case)
+    static String VALID_PYRAMID_WORD = "banana";                    // Positive test data supplied
+    static String NON_PYRAMID_BY_DUPLICATES = "bannaanna";          // Positive test data with duplicate values
+    static String INVALID_PYRAMID_WORD = "bandana";                 // Negative test data supplied
+    static String EMPTY_STRING = "";                                // Empty test data (error case)
 
     private PyramidTestConstants() {}
 }
